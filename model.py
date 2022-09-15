215d82704aa3ab6dc7b26d67d8c90fa67bcce2cb
aksel3007 <aksel.s.madsen@gmail.com>
2022-09-15 15:27:58 +0200

    TB_run_27 training acc > 99%

--- model.py
+++ model.py
@@ -29,6 +29,7 @@ class LSTM_Model(LightningModule):
         self.fc = nn.Linear(128 * self.mfccs_pr_window, 5 * self.mfccs_pr_window)
         self.flatten = nn.Flatten()
         self.output = nn.Sigmoid()
+        self.sm = nn.Softmax(dim=2)
         self.lr = 0.001
         self.batch_size = 64
         self.loss = nn.CrossEntropyLoss()
@@ -46,6 +47,7 @@ class LSTM_Model(LightningModule):
         x = self.output(x)
         #view as (batch_size, mfccs_pr_window, 5)
         x = x.view(-1, self.mfccs_pr_window, 5)
+        x = self.sm(x)
         
         return x
     
@@ -59,8 +61,9 @@ class LSTM_Model(LightningModule):
     
     def training_step(self, batch, batch_nb):
         x, y = batch
-        loss = self.loss(self(x), y)
-        preds = self(x)        
+        preds = self(x)
+        loss = self.loss(preds.flatten(end_dim = 1), y.flatten(end_dim = 1)) # Flatten to collect batch dimension and mfccs_pr_window dimension
+                
         
         # Use argmax to get the index of the highest value in the output vector along the 2. dimension (the 5 classes)
         # Then use flatten to get a 1D tensor with every guess
@@ -77,7 +80,7 @@ class LSTM_Model(LightningModule):
     
     def validation_step(self, batch, batch_nb):
         x, y = batch
-        loss = self.loss(self(x), y)
+        loss = self.loss(self(x).flatten(end_dim = 1), y.flatten(end_dim = 1))
         self.log("val_loss",loss)
         return {'val_loss': loss, 'log': {'val_loss': loss}}
     
@@ -117,7 +120,7 @@ if False: #for testing. Debugging doesn't work with separate files in this case?
     early_stop_callback = EarlyStopping(monitor='val_loss', min_delta=0.00, patience=5, verbose=True, mode='min')
 
     model = LSTM_Model('data').to(device)
-    trainer = Trainer(max_epochs=100, min_epochs=1, auto_lr_find=False, auto_scale_batch_size=False, callbacks=[early_stop_callback],enable_checkpointing=False)
+    #trainer = Trainer(max_epochs=100, min_epochs=1, auto_lr_find=False, auto_scale_batch_size=False, callbacks=[early_stop_callback],enable_checkpointing=False)
     trainer = Trainer(max_epochs=100, min_epochs=1, auto_lr_find=False, auto_scale_batch_size=False,enable_checkpointing=False)
     trainer.tune(model)
 
